e7a0ae1b0042dd9948ffd5048e2109aa4d98ffff
RakeshRaj97 <grakesh0301@gmail.com>
2020-07-04 20:03:06 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import os
+import torch
+
+import pretrainedmodels
+import albumentations
+import numpy as np
+import pandas as pd
+
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset, DataLoader
+from torch.cuda import amp
+
+from wtfml.data_loaders.image import ClassificationLoader
+
+class SEResNext50_32x4d(nn.Module):
+    def __init__(self, pretrained="imagenet"):
+        super(SEResNext50_32x4d, self).__init__()
+        #base model
+        self.model = pretrainedmodels.__dict__[
+            "se_resnext50_32x4d"
+        ](pretrained=pretrained)
+        self.out = nn.Linear(2048, 1)
+
+    def forward(self, image):
+        bs, _, _ = image.shape
+        x = self.model.features(image)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.reshape(bs, -1)
+        out = self.out(x)
+        return out
+
+def run(fold):
+    training_data_path = "/fred/oz138/test/kaggle/data/ssim/jpeg/melanoma-cancer-image-classification-kaggle-using-PyTorch/train_resized"
+    df = pd.read_csv("rgopala@farnarkle2:/fred/oz138/test/kaggle/data/ssim/jpeg/melanoma-cancer-image-classification-kaggle-using-PyTorch/train_folds.csv")
+    device = "cuda"
+    epochs = 50
+    train_bs = 32
+    valid_bs = 16
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.244, 0.255)
+
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+
+    train_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+
+    valid_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+
+    train_images = df_train.image_name.values.tolist()
+    train_images = [os.path.join(training_data_path, i + ".jpg") for i in train_images]
+    train_targets = df_train.target.values
+
+    valid_images = df_valid.image_name.values.tolist()
+    valid_images = [os.path.join(training_data_path, i + ".jpg") for i in valid_images]
+    valid_targets = df_valid.target.values
+
+    train_dataset = ClassificationLoader(
+        image_paths=train_images,
+        targets=train_targets,
+        resize=None,
+        augmentations=train_aug
+    )
+
+    train_loader = torch.utils.data.DataLoader()(
+        train_dataset,
+        batch_size=train_bs,
+        shuffle=True,
+        num_workers=4
+    )
+
+    valid_dataset = ClassificationLoader(
+        image_paths=valid_images,
+        targets=valid_targets,
+        resize=None,
+        augmentations=valid_aug
+    )
+
+    train_loader = torch.utils.data.DataLoader()(
+        valid_dataset,
+        batch_size=valid_bs,
+        shuffle=False,
+        num_workers=4
+    )
+
+    model = SEResNext50_32x4d(pretrained="imagenet")
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        patience=3,
+        mode="max"
+    )
+
+    model, optimizer = amp.initialize(
+        model,
+        optimizer,
+        opt_level="01",
+        verbosity=0
+    )
+
+    
